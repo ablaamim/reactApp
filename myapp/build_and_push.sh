4a1825b390930f38cb5bf2e7ac7b6af216208347
Abdessamad Laamimi <55363164+ablaamim@users.noreply.github.com>
2024-06-20 09:45:17 +0000

    deployment manifest

--- myapp/build_and_push.sh
+++ myapp/build_and_push.sh
@@ -8,10 +8,6 @@ docker tag simlab-react-image ablaamim/simlab-react-image
 
 sleep 2;
 
-docker login -u ablaamim -p dckr_pat_bKrTZlGMQbnef2meM6DgIgQuEIw
-
-sleep 2;
-
 docker push ablaamim/simlab-react-image
 
 #docker login
